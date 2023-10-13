2dc6b1e33d33279e59a04e5adf816a4793f98464
karodriguess <karinyrodrigues2004@gmail.com>
2023-10-12 21:47:41 -0300

    linhas/mapa

---
+++ frontend/Home/js/script.js
@@ -0,0 +1,7 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.titulo-principal', {duration:1000});
+sr.reveal('.cabecalho', {duration:1000});
+sr.reveal('.cta', {duration:1000});
+sr.reveal('.linhas', {duration:2000});
+sr.reveal('.mapa', {duration:2000});
