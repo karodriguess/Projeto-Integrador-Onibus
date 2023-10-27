caaffe10ed20d2677417b51cc0dd9a2a9da6f4b2
karodriguess <karinyrodrigues2004@gmail.com>
2023-10-27 16:24:05 -0300

    commit

--- frontend/public/js/script.js
+++ frontend/public/js/script.js
@@ -6,3 +6,30 @@ sr.reveal('.cta', {duration:1000});
 sr.reveal('.linhas', {duration:2000});
 sr.reveal('.mapa', {duration:2000});
 sr.reveal('.recarga', {duration:2000});
+sr.reveal('.formulario', {duration:2000});
+
+document.getElementById('btn-send').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    
+    alert('Recarga com sucesso. Aproveite seu cartão Smart!'); 
+});
+
+document.getElementById('menu').addEventListener('click', function(event) {
+    if (event.target.tagName === 'A') {
+        event.preventDefault(); // Evita o comportamento padrão do link
+
+        const targetId = event.target.getAttribute('href').substring(2); // Obtém o ID da seção alvo
+
+        const targetElement = document.getElementById(targetId); // Obtém o elemento alvo
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth' // Rolagem suave
+            });
+        }
+    }
+});
+
+
+
