11b361f8ce2a5d2f05040aa7452669065d05dc3d
karodriguess <karinyrodrigues2004@gmail.com>
2023-10-30 17:58:50 -0300

    commit

--- fe/routes/users.js
+++ fe/routes/users.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  res.sendFile("index.html", {root:"views"})
+  res.send('respond with a resource');
 });
 
 module.exports = router;
