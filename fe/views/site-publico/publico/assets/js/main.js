14bc552c747130a8b614efd9297d2e1217ad93b4
karodriguess <karinyrodrigues2004@gmail.com>
2023-11-19 21:56:10 -0300

    front-pb

--- fe/views/site-publico/publico/assets/js/main.js
+++ fe/views/site-publico/publico/assets/js/main.js
@@ -154,3 +154,12 @@
 
     
 })();
+
+
+let text = document.getElementById('text');
+
+window.addEventListener('scroll' ,function(){
+     let.value=this.window.scrollY;
+     
+    text.style.marginRight = value * 4 + 'px';
+})
