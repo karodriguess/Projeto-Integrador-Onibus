898185f220a11f2cc4a44b3b78401861b5ad6e70
= <o.ryan@aluno.ifsp.edu.br>
2023-12-04 00:22:12 -0300

    catraca

--- fe/routes/onibus/onibus.js
+++ fe/routes/onibus/onibus.js
@@ -12,25 +12,9 @@ router.get('/', function(req, res, next) {
 router.get('/aprovado', function(req, res, next) {
     res.render('onibus/approved');
 });
-
-// // http://localhost:3000/onibus/aprovado/deficiente
-// router.get('/aprovado/deficiente', function(req, res, next) {
-//     res.render('onibus/approved2');
-// });
-
-// // http://localhost:3000/onibus/aprovado/estudante
-// router.get('/aprovado/estudante', function(req, res, next) {
-//     res.render('onibus/approved3');
-// });
-
 // http://localhost:3000/onibus/recusado
 router.get('/recusado', function(req, res, next) {
     res.render('onibus/error');
-});
-
-// http://localhost:3000/onibus/recusado/estudante
-router.get('/recusado/estudante', function(req, res, next) {
-    res.render('onibus/error2');
 });
 
 module.exports = router;
